dfbce744b83c84033b8324c101bb486ed0e4090d
Atif-BE <atifummerkhalil@gmail.com>
2023-03-10 12:54:31 +0100

    Created the base added bg

---
+++ portfolio/src/styles/appStyles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background: url("/bg.jpg");
+    height: 100vh;
+    overflow-y: auto;
+    scroll-behavior: smooth;
+    scroll-snap-type: y mandatory;
+`
