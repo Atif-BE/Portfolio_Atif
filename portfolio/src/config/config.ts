584822f6f5645b2d46193601b4b81487200ec60e
Atif-BE <atifummerkhalil@gmail.com>
2023-03-12 14:26:20 +0100

    Added Form component and emailjs

---
+++ portfolio/src/config/config.ts
@@ -0,0 +1,3 @@
+export const SERVICE_ID = "service_zy7s8fb";
+export const TEMPLATE_ID = "template_kl7d8h6";
+export const USER_ID = "wAC3mPEguzEva647K";
--- portfolio/src/reusablecomponents/Form.tsx
+++ portfolio/src/reusablecomponents/Form.tsx
@@ -1,5 +1,29 @@
-const Form = () => {
+import emailjs from "@emailjs/browser";
+import { TEMPLATE_ID, SERVICE_ID, USER_ID } from "../config/config.js"
 
+const Form = () => {
+    const sendMail = (e: any) => {
+        e.preventDefault();
+
+        emailjs.sendForm(SERVICE_ID,TEMPLATE_ID, e.target, USER_ID)
+            .then(
+
+            )
+
+            e.target.reset();
+    }
+
+    return(
+        <form>
+            <label>Name</label>
+            <input type='text' placeholder='Your Name' name='name' required></input>
+            <label>E-mail</label>
+            <input type='email' placeholder='Your Email' name='email' required></input>
+            <label>Message</label>
+            <textarea placeholder='Coding a Message...' name='message' rows={5} required></textarea>
+            <button type='submit'>Send Message</button>
+        </form>
+    )
 }
 
 export default Form;
